57c4da35bb345b77e4ef667c69c04bf0622d1e72
allupfortae <avantaejacksoncontact@gmail.com>
2022-07-20 22:00:16 -0400

    modified files on 07 20 2022

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,8 +5,11 @@ import "./Home.css";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="home">
+      <div className="homeContainer">
+        <h1>Learning anything with groups and smart AI training.</h1>
+        <h3>REAL WORLD PROJECTS</h3>
+      </div>
     </div>
   );
 };

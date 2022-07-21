c5159191f18bd2f3d2d779800eb7778c3a906619
allupfortae <avantaejacksoncontact@gmail.com>
2022-07-21 18:56:37 -0400

    modified files on 07 21 2022 6:56pm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import QuickAcces from "./components/QuickAcces";
-import Home from "./pages/Home";
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Navbar />
+      <Home />
     </div>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,28 @@ import React from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div className="flex items-center relative NavBar h-32">
+      <div className="container ml-3 px-4 flex items-center mt-8">
+        <h1 className="text-3xl font-bold tracking-wide ">Central</h1>
+        <input
+          className="ml-32 w-1/2 px-5 py-3 rounded-full outline outline-gray-500"
+          type="text"
+          placeholder="Search for anything"
+        />
+        <div className="flex items-center gap-7 ml-12 text-sm text-gray-500 font-bold tracking tracking-wide">
+          <p>Groups</p>
+          <p>Plans</p>
+          <p>My learing</p>
+        </div>
+      </div>
+      <div className="mt-8  text-base flex gap-4 font-medium">
+        <span className="rounded-full items-center  text-white px-4 py-2 mr-10 text-lg font-bold bg-blue-500">
+          A
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;

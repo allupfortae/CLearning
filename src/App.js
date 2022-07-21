1e4ab6f14f03d16f99088458ba25771d02866af5
allupfortae <avantaejacksoncontact@gmail.com>
2022-07-21 13:20:00 -0400

    added tailwind css on 07 21 2022 at 1:20pm

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import QuickAcces from "./components/QuickAcces";
 import Home from "./pages/Home";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 };
 
 export default App;
